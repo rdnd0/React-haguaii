1f37d3840cf89119bfe18079a6697150f02899d6
David Redondo <rdnd201@gmail.com>
2019-02-27 17:28:19 +0100

    initial version with boiler plate

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Switch} from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute';
+import AnonRoute from './components/AnonRoute';
+import Navbar from './components/Navbar';
+import Private from './pages/Private';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import AuthProvider from './components/AuthProvider';
+
+
+class App extends Component {
+  render() {
+    return (
+      <AuthProvider>
+        <div className="container">
+          <h1>Basic React Authentication</h1>
+          <Navbar data='data' />
+          <Switch>
+            <AnonRoute path="/signup" component={Signup} />
+            <AnonRoute path="/login" component={Login} />
+            <PrivateRoute path="/private" component={Private} />
+          </Switch>
+        </div>
+      </AuthProvider>
+    )
+  }
+}
+
+export default App;
