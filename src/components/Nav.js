7cd682e13cec0bb88c4af2e75c4d5cee8e360a5a
David Redondo <rdnd201@gmail.com>
2019-03-02 14:34:31 +0100

    header animations added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,8 @@ export default class Nav extends Component {
     return (
       <nav>
         <a href="/"><img src="images/haguaii.png" alt="logo"/></a>
-        <a href="https://www.linkedin.com/in/davidredondo83/" target="_blank" rel="noopener noreferrer"><button>DONATE</button></a>
+        <a href="https://www.linkedin.com/in/davidredondo83/" target="_blank" rel="noopener noreferrer" className="donate-link">
+        <button className="donate-btn">DONATE</button></a>
       </nav>
     )
   }
