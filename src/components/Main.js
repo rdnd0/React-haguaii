8fdc5b816115cbb8419dc9c5c86d5b0150225540
David Redondo <rdnd201@gmail.com>
2019-03-06 18:51:26 +0100

    removed the api call from main as it is already in a component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './main.css';
-import Api from '../lib/shirts-service';
 import Nav from './Nav';
 import Header from './Header';
 import Configurator from './Configurator'
@@ -14,44 +13,13 @@ export default class Main extends Component {
     oldData: this.props.location.state,
   }
 
-  getIllustrations = () => {
-    Api.getIllustrations()
-      .then((data) => {
-        let components = {};
-        let arr = [];
-        let allData = data;   
-        data.forEach((object) => {
-          object.components.forEach((item) => {
-            components[item.short_name] = item.image
-          })
-        })
-        Object.keys(components).forEach(function(key) {
-          arr.push({
-            short_name: key,
-            image: components[key]
-          })
-        });
-        this.setState({
-          illustrations: arr,
-          isLoading: false,
-          allData
-        })
-      })
-      .catch((error) => {
-        console.log('ya got an error: ', error);
-      })
-  }
-
-  componentDidMount() {
-    this.getIllustrations();
-  }
 
   render() {
     return (
       <div id="top" className='main'>
         <Nav />
         <Header />
-        <Configurator illustrations={this.state.illustrations} allData={this.state.allData}/>
+        <Configurator/>
       </div>
     )
   }
