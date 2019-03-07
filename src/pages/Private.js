45520f65f3ba7d9b9886876000a9c56db957e0d5
David Redondo <rdnd201@gmail.com>
2019-03-07 13:42:16 +0100

    adjust after purchase screen

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import { withAuth } from '../components/AuthProvider';
+import Nav from '../components/Nav';
+
 class Private extends Component {
   render() {
     const { user } = this.props;
 
     return (
-      <div className='main-content'>
-        <h2>Thanks for trusting us {user.username}</h2>
-        <p>We have received your order and we will be working on your shirt right away!</p>
-        <a href="#top"><button className="back-btn" onClick={() => {this.props.history.push('/');}}>Back</button></a>
+      <div>
+        <Nav />
+        <div className='confirm-screen'>
+          <h1>Thanks for trusting us {user.username}</h1>
+          <p>We have received your order and we will be working on your shirt right away!</p>
+          <div className="confirm-logos">
+            <img src="/images/iconshirt.svg" alt="shirt"/>
+            <img src="/images/ilustrationicon.svg" alt="draw"/>
+            <img src="/images/velabcnicon.svg" alt="bcn"/> 
+          </div>
+          <a href="#top"><button className="back-btn" onClick={() => {this.props.history.push('/');}}>Back</button></a>
+        </div>
       </div>
     )
   }
